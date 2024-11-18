c4eb2215b9efdfac651aa85ff30ff73c6276eeb8
iam-ogomide <ogomidesamuel10@gmail.com>
2024-11-18 09:04:45 +0100

    20%

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div id='tech' className='flex min-h-[70vh] w-full flex-col items-center justify-center gap-16 md:gap-32'>
+      <h1 className="text-4xl font-light text-white md:text-6xl">Stack</h1>
+
+      <div className="flex flex-wrap items-center justify-center gap-10 p-5"></div>
+    </div>
   )
 }
 
